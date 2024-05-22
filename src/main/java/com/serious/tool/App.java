3614f747d010cd67d5cc293a00bd948d1d07cf98
whcater <1010127369@qq.com>
2024-05-22 16:54:41 +0800

    default running use "web" folder as web root

--- src/main/java/com/serious/tool/App.java
+++ src/main/java/com/serious/tool/App.java
@@ -1,12 +1,17 @@
 package com.serious.tool;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 /**
  *
  */
+@SpringBootApplication
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Serious!" );
+        System.out.printf("running at %s%n",System.getProperty("user.dir"));
+        SpringApplication.run(App.class,args);
     }
 }
